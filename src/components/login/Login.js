0b5e25ec9c7e710b1fe76cdc1a8ea44781187a7a
Afzal Ahmed Pinjar <afzalap163@gmail.com>
2022-03-20 11:43:28 +0530

    login 1.0

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,16 +1,70 @@
 import Btn from "../button/Btn"
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
 import './login.css'
 
 const Login = () => {
 
-    var log = () => {
-        console.log("hello");
+    const [currentAccount, setCurrentAccount] = useState("");
+
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                console.log("Make sure you have metamask!");
+                return;
+            } else {
+                console.log("We have the ethereum object", ethereum);
+            }
+
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log("Found an authorized account:", account);
+                setCurrentAccount(account);
+            } else {
+                console.log("No authorized account found")
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                alert("Get MetaMask!");
+                return;
+            }
+
+            const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+            console.log("Connected", accounts[0]);
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, [])
 
     return (
         <div className="login">
-            <Btn text={"Connect Wallet to login"} func={log} />
-            <Btn text={"Register"} func={log} />
+            <div className="whitebg">
+                <p>Health Record Management System</p>
+            </div>
+            <div className="btn-grp">
+                <Btn text={"Connect Wallet to login"} func={connectWallet} />
+                <Btn text={"Register"} func={connectWallet} />
+            </div>
 
         </div>
     )

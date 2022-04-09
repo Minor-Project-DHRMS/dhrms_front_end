869401a16f96c8c7a4066a4215cadbac12626929
Sahil-123 <sahilnaikwadi123@gmail.com>
2022-04-09 18:04:58 +0530

    viewScan,viewReport,listScan,listReport,medicalReport
    viewScan,viewReport,listScan,listReport,medicalReport

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ import Hospital from "./Pages/hospital/Hospital";
 import Login from "./Pages/login/Login";
 import Patient from "./Pages/patient/Patient";
 import PatientReg from "./Pages/PatientRegistration/PatientReg";
+import PatientDetails from "./Pages/medicalReport/PatientDetails"
+import ViewScan from "./Pages/viewScans/ViewScan"
+import ListScan from "./Pages/listScans/ListScan"
+import ViewReport from "./Pages/viewReport/viewReport"
+import ListReport from "./Pages/listReport/ListReport"
+
 
 const App = () => {
   return (
     <Router>
       <div className="container">
         <Routes>
-          <Route path="/" element={<Patient />} />
+          <Route path="/" element={<PatientDetails />} />
           <Route path="/gov" element={<Government />} />
+          <Route path="/medicalReport" element={<PatientDetails />} />
+          <Route path="/viewScan" element={<ViewScan />} />
+          <Route path="/viewReport" element={<ViewReport />} />
+          <Route path="/listScan" element={<ListScan />} />
+          <Route path="/listReport" element={<ListReport />} />
         </Routes>
         {/* <PatientReg/> */}
       </div>
---
+++ src/Pages/viewScans/ViewScan.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ViewScan.css"
+//import Btn from "../../components/button/Btn";
+
+
+export default function ViewScan() {
+    return (
+
+        <>
+            <div className="headingScan">
+                Scaned Page
+            </div>
+            <div className="outerLayerforScan">
+
+                <div className="outerForScan">
+                    <img src={"./images/img3.jpg"} className="myimg" alt="">
+                    </img>
+                    <img src={"./images/img3.jpg"} className="myimg" alt="">
+                    </img>
+                    <img src={"./images/img3.jpg"} className="myimg" alt="">
+                    </img>
+                    <img src={"./images/img3.jpg"} className="myimg" alt="">
+                    </img>
+                </div>
+            </div>
+        </>
+    );
+}

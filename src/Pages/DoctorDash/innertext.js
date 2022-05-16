73ffb81bf3db67f8ad5ebe4d2dc91d95917a6dd2
Sahil-123 <sahilnaikwadi123@gmail.com>
2022-05-16 19:29:03 +0530

    changes in shinyas code

--- src/Pages/DoctorDash/innertext.js
+++ src/Pages/DoctorDash/innertext.js
@@ -1,12 +1,11 @@
 import React from "react";
-export default function intext(){
-    return(<nav>
-        <div className="details">
-        <h1 className="tx1">Dr.Sahil.L.Naikwadi</h1>
-    <p className="tx2">Cardiologist</p>
-    <p className="tx3">MBBS ,MD</p>
-    <p className="tx4
-    ">Hospital Name : SDM Medical College Dharwad</p>
-    </div>
-    </nav>);
+export default function intext() {
+    return (
+        <div className="docheading">
+            <h1 className="tx1">Dr.Sahil.L.Naikwadi</h1>
+            <p className="tx1">Cardiologist</p>
+            <p className="tx1">MBBS ,MD</p>
+            <p className="tx1">Hospital Name : SDM Medical College Dharwad</p>
+        </div>
+    );
 }
--- src/Pages/DoctorDash/topdash.js
+++ src/Pages/DoctorDash/topdash.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Intxt from "./innertext";
 
-export default function topbar(){
-    return(<div className="topbar1">
-         <div className="topbar2">
-         <p className="Obj"></p>  <Intxt/>
-    </div>
-    </div>
+export default function topbar() {
+    return (
+        <div className="topbar1">
+            <div className="topbar2">
+                <p className="Obj"></p>  
+                <Intxt />
+            </div>
+        </div>
     );
 }

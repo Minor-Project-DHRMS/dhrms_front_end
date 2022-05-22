4c1ac572d47a9639f2452e3d71938ba5b65eedf4
Nandeesh B K <70832915+Nandeesh68@users.noreply.github.com>
2022-05-22 15:13:12 +0530

    Update doctor dashboard

--- src/Pages/DoctorDash/topdash.js
+++ src/Pages/DoctorDash/topdash.js
@@ -3,37 +3,33 @@ import { getDoctorDetails } from "../../functions/Dhrms";
 import { getAccountAddress } from "../../services/AccountDetails";
 
 export default function Topbar() {
-    const [docdata, setData] = useState({});
-    const getDetails = async () => {
-        try {
-            const acc = getAccountAddress();
-            await getDoctorDetails(acc).then((x) => {
+  const [docdata, setData] = useState({});
+  const getDetails = async () => {
+    try {
+      const acc = getAccountAddress();
+      await getDoctorDetails(acc).then((x) => {
+        setData(x);
+        console.log(x);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-                setData(x);
-                console.log(x);
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  useEffect(() => {
+    getDetails();
+  }, []);
 
-
-    useEffect(() => {
-        getDetails();
-    }, []);
-
-    // console.log(docdata.hos.HID);
-    return (
-        <div className="topbar1">
-            <div className="topbar2">
-                <img className="Obj" src={docdata.photo}></img>
-                <div className="docheading">
-                    <h1 className="tx1">{docdata.doctorName}</h1>
-                    <p className="tx1">{docdata.department}</p>
-                    <p className="tx1">{docdata.qualification}</p>
-                    {/* <p className="tx1">{console.log(docdata)}</p> */}
-                </div>
-            </div>
+  // console.log(docdata.hos.HID);
+  return (
+    <div className="topbar1">
+      <div className="topbar2">
+        <img className="Obj" src={docdata.photo}></img>
+        <div className="docheading">
+          <h1 className="tx1">{docdata.doctorName}</h1>
+          <p className="tx1">{docdata.department}</p>
+          <p className="tx1">{docdata.qualification}</p>
+          {/* <p className="tx1">{console.log(docdata)}</p> */}
         </div>
       </div>
     </div>

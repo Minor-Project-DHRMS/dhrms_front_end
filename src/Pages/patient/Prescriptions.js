1483e19de8dd05e91d384b905248c594f1ad80db
Nandeesh B K <70832915+Nandeesh68@users.noreply.github.com>
2022-05-22 16:46:04 +0530

    Patient dashboard updates

--- src/Pages/patient/Prescriptions.js
+++ src/Pages/patient/Prescriptions.js
@@ -4,27 +4,45 @@ import { ReactSession } from "react-client-session";
 import Btn from "../../components/button/Btn";
 import "./patient.css";
 import mainLogo from "../patient/logo192.png";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { QRCodeGen } from "../../components/QRPage/QRCodeGen";
 import { useNavigate } from "react-router-dom";
+import { dateFormate } from "../../services/DateFormate";
 
-const Prescriptions = ({ desc }) => {
+const Prescriptions = () => {
   let navigate = useNavigate();
+  const location = useLocation();
 
-  ReactSession.setStoreType("sessionStorage");
-  const currentAccount = ReactSession.get("currentAccount");
+  console.log(location.state.record);
 
+  const [data, setData] = useState(location.state.record);
+  const nums = [1, 2, 3, 3, 4];
   return (
     <div className="patient">
       <div className="bio_pat">
         <div>My Prescriptions</div>
       </div>
 
-      <div className="pt_align_ht">
-        {/* <div>Hello</div>
-        <div>Hello</div> */}
-        <div className="bio-hos bio-user">
-          <div className="list-items">{desc}</div>
+      {/* <div className="pt_align_ht">
+        <div className="bio-hos bio-user  bio-pat">
+          <div className="list-items">{data.recordDetails.prescription}</div>
+        </div>
+      </div> */}
+      <div className="bio-hos bio-user bio-pres">
+        <div className="list-items">
+          {data.map((record, index) => {
+            return (
+              <div key={index} className="item item-size">
+                <div className="align-vh">
+                  <div className="pres_font">
+                    Date : {dateFormate(record.recordDetails.timeStamp)}
+                  </div>
+                  <hr />
+                  <div className="pres_font">{record.recordDetails.prescription}</div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
